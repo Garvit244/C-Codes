b4e34461724274567c9ea1c3e3b5e35fd71830bd
garvitbansal <garvitbansal244@gmail.com>
2014-09-11 15:38:45 +0530

    Inital commit

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+		int num = 1000000,c=0;
+		 for(int i=1;i*i<=num;i++)
+		 {
+            c++;
+        }
+        cout<<c<<endl;
+	return 0;
+}
---
+++ try.java
@@ -0,0 +1,12 @@
+public class try
+{
+	public static void main(String[] args) 
+	{
+		int num = 10;
+		 for(int i=1,c=0;i*i<=bulbsCount;i++)
+		 {
+            c++;
+        }
+        System.out.println(c);
+	}
+}
